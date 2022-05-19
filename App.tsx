13339d572ddcd8748ffc27c9bf58a4fe5b3b7593
Marcos Guibso <mgss.lic17@uea.edu.br>
2022-05-18 22:45:20 -0300

    Adicionando fontes personalizadas ao tema global

--- App.tsx
+++ App.tsx
@@ -1,11 +1,29 @@
 import React from 'react';
+import AppLoading from 'expo-app-loading';
 import { ThemeProvider } from 'styled-components';
+
+import {
+  useFonts,
+  Poppins_400Regular,
+  Poppins_500Medium,
+  Poppins_700Bold
+} from '@expo-google-fonts/poppins';
 
 import { Dashboard } from './src/components/screens/Dashboard';
 import theme from './src/global/styles/theme';
 
 
 export default function App() {
+  const [fontsLoaded] = useFonts({
+    Poppins_400Regular,
+    Poppins_500Medium,
+    Poppins_700Bold
+  });
+
+  if(!fontsLoaded){
+    return <AppLoading/>
+  }
+
   return (
     <ThemeProvider theme={theme}>
       <Dashboard />
