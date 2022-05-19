df60a90de684e3d275bc4edaedd00ad48383ee19
Marcos Guibso <mgss.lic17@uea.edu.br>
2022-05-18 21:43:47 -0300

    Adicionando Styled-Components

--- App.tsx
+++ App.tsx
@@ -1,21 +1,13 @@
-import { StatusBar } from 'expo-status-bar';
 import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
-import { Welcome } from './src/components/Welcome';
+import { Dashboard } from './src/components/screens/Dashboard';
+
 
 export default function App() {
   return (
-    <View style={styles.container}>
-      <Welcome title='App Criado com Bare workflow e typescript'/>
-    </View>
+
+    <Dashboard />
+
   );
 }
 
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
-});
+
