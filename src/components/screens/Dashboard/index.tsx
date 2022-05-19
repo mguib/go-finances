de8b3d9a4ccf7b886cfc8970e3567629559a0630
Marcos Guibso <mgss.lic17@uea.edu.br>
2022-05-18 23:45:44 -0300

    Adicionando poporções de medidas e icones utilizando expo

--- src/components/screens/Dashboard/index.tsx
+++ src/components/screens/Dashboard/index.tsx
@@ -1,16 +1,34 @@
 import React from "react";
 
+
 import {
     Container,
-    Title
+    Header,
+    UserInfo,
+    UserWrapper,
+    Photo,
+    User,
+    UserGreeting,
+    UserName,
+    Icon,
 } from './styles';
 
 export function Dashboard(){
     return(
         <Container>
-            <Title>
-                Dashboard
-            </Title>
+            <Header>
+                <UserWrapper>
+                    <UserInfo>
+                        <Photo 
+                            source={{uri:'https://avatars.githubusercontent.com/u/43528733?v=4' }}/>
+                        <User>
+                            <UserGreeting>Olá,</UserGreeting>
+                            <UserName>Guibson</UserName>
+                        </User>
+                    </UserInfo>
+                    <Icon name="power" />
+                </UserWrapper>
+            </Header>
         </Container>
     )
 }
